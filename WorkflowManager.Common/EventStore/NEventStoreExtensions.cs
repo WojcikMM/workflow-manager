247952b01561e071f7dc9b125bc1e4ef815db2c7
Wojcik Michal <m.mwojcik@outlook.com>
2019-11-30 23:57:34 +0100

    Add sql persistance for event store

--- WorkflowManager.Common/EventStore/NEventStoreExtensions.cs
+++ WorkflowManager.Common/EventStore/NEventStoreExtensions.cs
@@ -1,20 +1,50 @@
 ﻿using CQRS.Template.Domain.Storage;
 using Microsoft.Extensions.DependencyInjection;
 using NEventStore;
+using NEventStore.Logging;
+using System.Data.SqlClient;
+using NEventStore.Serialization.Json;
+using Microsoft.Extensions.Configuration;
+using System;
 
 namespace WorkflowManager.Common.EventStore
 {
     public static class NEventStoreExtensions
     {
-        public static void AddEventStore(this IServiceCollection serviceCollection)
+        public static void AddEventStore(this IServiceCollection serviceCollection, LogLevel logLevel = LogLevel.Info, string connectionStringName = "EventStoreDatabase")
+        {
+            string connectionString = null;
+            using (ServiceProvider serviceProvider = serviceCollection.BuildServiceProvider())
+            {
+                IConfiguration configuration = serviceProvider.GetService<IConfiguration>();
+                connectionString = configuration.GetConnectionString(connectionStringName);
+            }
+            if (connectionString is null)
+            {
+                throw new ApplicationException("Cannot find event store connectionString value.");
+            }
+
+            serviceCollection.AddSingleton(typeof(IRepository<>), typeof(AggregateRespository<>));
+
+            serviceCollection.AddTransient<IStoreEvents>(service => Wireup
+                .Init()
+                .LogToConsoleWindow(logLevel)
+                .UsingSqlPersistence(SqlClientFactory.Instance, connectionString)
+                .WithDialect(new NEventStore.Persistence.Sql.SqlDialects.MsSqlDialect())
+                .InitializeStorageEngine()
+                .UsingJsonSerialization()
+                .Build());
+
+        }
+
+        public static void AddInMemoryEventStore(this IServiceCollection serviceCollection, LogLevel logLevel = LogLevel.Info)
         {
             serviceCollection.AddSingleton(typeof(IRepository<>), typeof(AggregateRespository<>));
             serviceCollection.AddTransient<IStoreEvents>(service => Wireup
                 .Init()
-                .LogToConsoleWindow(NEventStore.Logging.LogLevel.Info)
+                .LogToConsoleWindow(logLevel)
                 .UsingInMemoryPersistence()
                 .Build());
-
         }
     }
 }
