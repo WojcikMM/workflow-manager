c41293d3257aca11e0a35a16bfc4305c8af86da8
Wojcik Michal <m.mwojcik@outlook.com>
2019-12-09 21:16:35 +0100

    fix problem with Database.EnsureCreated()

--- src/Services/ProcessService/WorkflowManager.ProcessService.ReadModel/ReadDatabase/ProcessesContext.cs
+++ src/Services/ProcessService/WorkflowManager.ProcessService.ReadModel/ReadDatabase/ProcessesContext.cs
@@ -4,7 +4,10 @@ namespace WorkflowManager.ProcessService.ReadModel.ReadDatabase
 {
     public class ProcessesContext : DbContext
     {
-        public ProcessesContext(DbContextOptions<ProcessesContext> options) : base(options) { }
+        public ProcessesContext(DbContextOptions<ProcessesContext> options) : base(options) 
+        {
+            this.Database.EnsureCreated();
+        }
 
         public DbSet<ProcessModel> Processes { get; set; }
     }
