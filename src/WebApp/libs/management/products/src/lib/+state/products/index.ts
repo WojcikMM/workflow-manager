5a784bdc5dbd0016f153ff61f044af91a398c721
Wojcik Michal <m.mwojcik@outlook.com>
2020-10-20 23:04:52 +0200

    start working on management module

---
+++ src/WebApp/libs/management/products/src/lib/+state/products/index.ts
@@ -0,0 +1,6 @@
+export * from './products.facade';
+export * from './products.effects';
+export * from './products.actions';
+export * from './products.models';
+export * from './products.reducer';
+export * from './products.selectors';
